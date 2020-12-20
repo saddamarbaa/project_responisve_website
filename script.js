de11e21ea58302d219d0ecd966f087c849621ec4
saddamarbaa <saddamarbaas@gmail.com>
2020-12-20 16:43:31 +0700

    Responsive Website Using CSS Grid Final Build

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const navButton = document.querySelector("button[aria-expanded]");
 
 function toggleNav({ target }) {
   // console.log("test");
+
   const expanded = target.getAttribute("aria-expanded") === "true" || false;
   // console.log(expanded);
   navButton.setAttribute("aria-expanded", !expanded);
